fd170d66c8896a12f981c4b023bdb99fbc82aed5
DESKTOP-HGO5QJ2\bunch <buncha102828@gmail.com>
2018-05-28 19:13:25 +0900

    動作を作った

--- TebasakiChronicle/src/Object/Enemy/EnemyType/CharacterBehavior.cpp
+++ TebasakiChronicle/src/Object/Enemy/EnemyType/CharacterBehavior.cpp
@@ -8,7 +8,7 @@ CharacterBehavior::CharacterBehavior(int moveNum)
 }
 CharacterBehavior::~CharacterBehavior()
 {
-	delete behavior;
+	Memory::SafeDelete(behavior);
 }
 
 //-----------------------------------------------------------------------------
@@ -36,6 +36,38 @@ void CharacterBehavior::SetBehavior(int moveNum)
 	case 4:		//前方に攻撃する
 		behavior = new Behavior_FrontAttack();
 		break;
+
+	case 5:		//重力の有無を切り替える
+		behavior = new Behavior_SwitchingGravity();
+		break;
+
+	case 6:		//重力を無効にして浮く
+		behavior = new Behavior_DisableGravityAndFloat();
+		break;
+
+	case 7:		//重力を一時的に倍にする
+		behavior = new Behavior_ToDoubleGravity();
+		break;
+
+	case 8:		//重力を一時的に半分にする
+		behavior = new Behavior_ToHarfGravity();
+		break;
+
+	case 9:		//空中で上下に揺れる
+		behavior = new Behavior_AirIdle();
+		break;
+
+	case 10:	//空中で揺れながら向いている方向に移動する
+		behavior = new Behavior_AirMovementToDirection();
+		break;
+
+	case 11:	//Y座標の真ん中から弾を発射する
+		behavior = new Behavior_ShotBulletFromMiddle();
+		break;
+
+	case 12:	//重力アリの爆弾を投げる
+		behavior = new Behavior_ThrowBomb();
+		break;
 	}
 }
 //-----------------------------------------------
@@ -60,7 +92,7 @@ void CharacterBehavior::Finalize(TemporaryCollisionManager& tempmanager, Status&
 }
 
 
-/* テンプレ
+/* 動作追加のためのテンプレ
 
 //-----------------------------------------------
 //
@@ -139,7 +171,7 @@ void Behavior_MovementToDirection::Finalize(TemporaryCollisionManager& tempmanag
 }
 
 //-----------------------------------------------
-//一回だけジャンプする
+//無条件に一回だけジャンプする
 void Behavior_OnceJump::Initialize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
 {
 	move.JumpOperation();
@@ -184,6 +216,173 @@ void Behavior_FrontAttack::Action(TemporaryCollisionManager& tempmanager, Status
 	//何もしない
 }
 void Behavior_FrontAttack::Finalize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	//何もしない
+}
+
+//-----------------------------------------------
+//重力の有無を切り替える
+void Behavior_SwitchingGravity::Initialize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	if (move.GetGravity() > 0)
+	{
+		move.SetGravity(0);
+	}
+	else
+	{
+		move.SetDefaultGravity();
+	}
+}
+void Behavior_SwitchingGravity::Action(TemporaryCollisionManager& tempmanager, Status& status, Move& move, const TimeCount& timeCnt)
+{
+	//何もしない
+}
+void Behavior_SwitchingGravity::Finalize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	//何もしない
+}
+
+//-----------------------------------------------
+//重力を無効にして浮く
+void Behavior_DisableGravityAndFloat::Initialize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	move.SetGravity(0);
+}
+void Behavior_DisableGravityAndFloat::Action(TemporaryCollisionManager& tempmanager, Status& status, Move& move, const TimeCount& timeCnt)
+{
+	move.GetMoveVec().y += move.GetAddVec();
+}
+void Behavior_DisableGravityAndFloat::Finalize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	//何もしない
+}
+
+//-----------------------------------------------
+//重力を一時的に二倍にする
+void Behavior_ToDoubleGravity::Initialize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	move.SetGravity(move.GetGravity() * 2.f);
+}
+void Behavior_ToDoubleGravity::Action(TemporaryCollisionManager& tempmanager, Status& status, Move& move, const TimeCount& timeCnt)
+{
+	//何もしない
+}
+void Behavior_ToDoubleGravity::Finalize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	move.SetDefaultGravity();
+}
+
+//-----------------------------------------------
+//重力を一時的に半分にする
+void Behavior_ToHarfGravity::Initialize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	move.SetGravity(move.GetGravity() / 2.f);
+}
+void Behavior_ToHarfGravity::Action(TemporaryCollisionManager& tempmanager, Status& status, Move& move, const TimeCount& timeCnt)
+{
+	//何もしない
+}
+void Behavior_ToHarfGravity::Finalize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	move.SetDefaultGravity();
+}
+
+//-----------------------------------------------
+//空中で上下に揺れる
+void Behavior_AirIdle::Initialize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	//何もしない
+}
+void Behavior_AirIdle::Action(TemporaryCollisionManager& tempmanager, Status& status, Move& move, const TimeCount& timeCnt)
+{
+	move.GetMoveVec().y += sin(K_Math::DegToRad((float)timeCnt.GetNowCntTime() * 4.f)) / move.GetAddVec();
+}
+void Behavior_AirIdle::Finalize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	//何もしない
+}
+
+//-----------------------------------------------
+//空中で揺れながら向いている方向に移動する
+void Behavior_AirMovementToDirection::Initialize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	//何もしない
+}
+void Behavior_AirMovementToDirection::Action(TemporaryCollisionManager& tempmanager, Status& status, Move& move, const TimeCount& timeCnt)
+{
+	move.GetMoveVec().y += sin(K_Math::DegToRad((float)timeCnt.GetNowCntTime() * 4.f)) / move.GetAddVec();
+	if (status.GetDirection() == status.Right)
+	{
+		move.GetMoveVec().x += move.GetAddVec();
+	}
+	else
+	{
+		move.GetMoveVec().x -= move.GetAddVec();
+	}
+}
+void Behavior_AirMovementToDirection::Finalize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	//何もしない
+}
+
+//-----------------------------------------------
+//Y座標の真ん中から前方に弾を発射する
+void Behavior_ShotBulletFromMiddle::Initialize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	K_Math::Vector3 relative(16, 0, 0);
+	if (status.GetDirection() == Status::Direction::Left)
+		relative.x *= -1;
+
+	//攻撃用コリジョンを作成
+	tempmanager.CreateTemporaryCollision(
+		CC::CreateBoxShape(10.f, 10.f, 10.f),
+		CollisionMask::Non,
+		CollisionMask::TakeDamagePlayer,
+		status.GetPos() + relative,
+		K_Math::Vector3(10, 0, 0),
+		status.GetDirection(),
+		status.GetAttackPoint(),
+		60,
+		0,
+		false,
+		false);
+}
+void Behavior_ShotBulletFromMiddle::Action(TemporaryCollisionManager& tempmanager, Status& status, Move& move, const TimeCount& timeCnt)
+{
+	//何もしない
+}
+void Behavior_ShotBulletFromMiddle::Finalize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	//何もしない
+}
+
+//-----------------------------------------------
+//重力アリの爆弾を投げる
+void Behavior_ThrowBomb::Initialize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
+{
+	K_Math::Vector3 relative(16, 0, 0);
+	if (status.GetDirection() == Status::Direction::Left)
+		relative.x *= -1;
+
+	//攻撃用コリジョンを作成
+	tempmanager.CreateTemporaryCollision(
+		CC::CreateBoxShape(10.f, 10.f, 10.f),
+		CollisionMask::Non,
+		CollisionMask::TakeDamagePlayer,
+		status.GetPos() + relative,
+		K_Math::Vector3(10, 10, 0),
+		status.GetDirection(),
+		status.GetAttackPoint(),
+		180,
+		-0.5f,
+		true,
+		false);
+}
+void Behavior_ThrowBomb::Action(TemporaryCollisionManager& tempmanager, Status& status, Move& move, const TimeCount& timeCnt)
+{
+	//何もしない
+}
+void Behavior_ThrowBomb::Finalize(TemporaryCollisionManager& tempmanager, Status& status, Move& move)
 {
 	//何もしない
 }
